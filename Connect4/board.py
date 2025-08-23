e023c7a0bd619e97b609c2df7285ea2eb14bca94
matthewfaler <matthewfaler1@gmail.com>
2025-08-23 16:05:45 -0400

    Board finished

--- Connect4/board.py
+++ Connect4/board.py
@@ -1,9 +1,12 @@
 import numpy
 
 class Board:
-    board = numpy.array(numpy.zeros((6, 7)))
+    board = numpy.full([6, 7], '◯', dtype='U1')
 
     def out(self):
         print("  A  B  C  D  E  F  G")
-        print(self.board)
+        print(numpy.array2string(self.board, separator="  ", formatter={'all': lambda x: x}).replace("[", " ").replace("]", ""))     # type: ignore
 
+if __name__ == "__main__":
+    b = Board()
+    b.out()
