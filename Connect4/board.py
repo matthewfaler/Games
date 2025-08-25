55743e3b4246e54e12d7054e75835e9a32d74f0e
matthewfaler <matthewfaler1@gmail.com>
2025-08-25 19:24:41 -0400

    Finished working connect4 game

--- Connect4/board.py
+++ Connect4/board.py
@@ -1,23 +1,70 @@
 import numpy
 
 colors = ('●', '○')
+below = 6
+right = 7
+left = -1
+above = -1
+
 class Board:
     def __init__(self):
-        self.board = numpy.full([6, 7], '·', dtype='U1')
+        self.board = numpy.full([below, right], '·', dtype='U1')
         self.player = 0 
         self.nextSpots = [5, 5, 5, 5, 5, 5, 5]
-
 
     def toggleColor(self):
         self.player = 1 - self.player
 
     def put(self, x: int):
-        if self.nextSpots[x] == -1:
+        if self.nextSpots[x] == above:
             print("\033[31mError: Column is full.\033[0m")
-            return
+            return False
         self.board[self.nextSpots[x]][x] = colors[self.player]
+        if self.checkVictory(x, self.nextSpots[x]):
+            return True
         self.toggleColor();
         self.nextSpots[x] -= 1
+        return False
+
+    def checkVictory(self, x: int, y: int):
+        hor, ver, diag, adiag = 1, 1, 1, 1
+        connect = 4
+        for i in range(1, connect):
+            if y + i >= below or self.board[y + i][x] != colors[self.player]: break
+            else: ver += 1
+        if ver >= connect: return True
+
+        for i in range(1, connect):
+            if x + i >= right or self.board[y][x + i] != colors[self.player]: break
+            else: hor += 1
+        if hor >= connect: return True
+        for i in range(1, connect):
+            if x - i <= left or self.board[y][x - i] != colors[self.player]: break
+            else: hor += 1
+        if hor >= connect: return True
+
+        for i in range(1, connect):
+            if x - i <= left or y - i <= above or self.board[y - i][x - i] != colors[self.player]: break
+            else: diag += 1
+        if diag >= connect: return True
+        for i in range(1, connect):
+            if x + i >= right or y + i >= below or self.board[y + i][x + i] != colors[self.player]: break
+            else: diag += 1
+        if diag >= connect: return True
+
+        for i in range(1, connect):
+            if x - i <= left or y + i >= below or self.board[y + i][x - i] != colors[self.player]: break
+            else: adiag += 1
+        if adiag >= connect: return True
+        for i in range(1, connect):
+            if x + i >= right or y - i <= above or self.board[y - i][x + i] != colors[self.player]: break
+            else: adiag += 1
+        if adiag >= connect: return True
+
+        return False
+
+    def checkFull(self):
+       return all(x == -1 for x in self.nextSpots)
 
     def out(self):
         print("  A  B  C  D  E  F  G")
--- Connect4/game.py
+++ Connect4/game.py
@@ -3,30 +3,28 @@ import board
 class Game:
     def __init__(self):
         self.board = board.Board()
-
+        self.finished = False
 
     def turn(self):
-        inp = input("Choose which column to play (A-G): ")
-        match inp:
-            case 'A':
-                self.board.put(0)
-            case 'B':
-                self.board.put(1)
-            case 'C':
-                self.board.put(2)
-            case 'D':
-                self.board.put(3)
-            case 'E':
-                self.board.put(4)
-            case 'F':
-                self.board.put(5)
-            case 'G':
-                self.board.put(6)
-            case _:
-                print("Please choose a valid option.")
+        inp = input("Choose which column to play (A-G): ").upper()
+        colmap = {'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6}
+
+        if inp not in colmap:
+            print("Please choose a valid option.")
+            return
+
+        won = self.board.put(colmap[inp])
+        if won:
+            self.board.out()
+            print(f"Player {board.colors[self.board.player]} wins!")
+            self.finished = True
+        elif self.board.checkFull():
+            self.board.out()
+            print("It's a draw!")
+            self.finished = True
 
 if __name__ == "__main__":
     g = Game()
-    while(True):
+    while not g.finished:
         g.board.out()
         g.turn()
