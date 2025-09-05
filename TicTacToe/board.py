02d9b755ac9cb429b493bfcdd632efdbc91b7f9b
matthewfaler <matthewfaler1@gmail.com>
2025-09-04 23:34:56 -0400

    refs #1: Working tic-tac-toe game with a few fixed bugs

--- TicTacToe/board.py
+++ TicTacToe/board.py
@@ -8,6 +8,9 @@ class Board:
         self.player = 0
 
     def put(self, x, y):
+        if self.board[y, x] != ' ':
+            print("\033[31mPlease select empty space.\033[0m")
+            return -1
         self.board[y, x] = x_or_o[self.player]
         self.togglePlayer()
         if self.checkVictory(): return 2
@@ -15,14 +18,14 @@ class Board:
         return 0
 
     def checkVictory(self):
-        if self.board[0][0] == self.board[0][1] and self.board[0][1] == self.board[0][2]: return True
-        if self.board[1][0] == self.board[1][1] and self.board[1][1] == self.board[1][2]: return True
-        if self.board[2][0] == self.board[2][1] and self.board[2][1] == self.board[2][2]: return True
-        if self.board[0][0] == self.board[1][0] and self.board[1][0] == self.board[2][0]: return True
-        if self.board[0][1] == self.board[1][1] and self.board[1][1] == self.board[2][1]: return True
-        if self.board[0][2] == self.board[1][2] and self.board[1][2] == self.board[2][2]: return True
-        if self.board[0][0] == self.board[1][1] and self.board[1][1] == self.board[2][2]: return True
-        if self.board[0][2] == self.board[1][1] and self.board[1][1] == self.board[2][0]: return True
+        if self.board[0][0] == self.board[0][1] and self.board[0][1] == self.board[0][2] and self.board[0][1] != ' ': return True
+        if self.board[1][0] == self.board[1][1] and self.board[1][1] == self.board[1][2] and self.board[1][1] != ' ': return True
+        if self.board[2][0] == self.board[2][1] and self.board[2][1] == self.board[2][2] and self.board[2][1] != ' ': return True
+        if self.board[0][0] == self.board[1][0] and self.board[1][0] == self.board[2][0] and self.board[1][0] != ' ': return True
+        if self.board[0][1] == self.board[1][1] and self.board[1][1] == self.board[2][1] and self.board[1][1] != ' ': return True
+        if self.board[0][2] == self.board[1][2] and self.board[1][2] == self.board[2][2] and self.board[1][2] != ' ': return True
+        if self.board[0][0] == self.board[1][1] and self.board[1][1] == self.board[2][2] and self.board[1][1] != ' ': return True
+        if self.board[0][2] == self.board[1][1] and self.board[1][1] == self.board[2][0] and self.board[1][1] != ' ': return True
         return False
 
     def checkFull(self):
@@ -39,6 +42,8 @@ class Board:
         print("  -----------")
         print(f" 3 {self.board[2][0]} | {self.board[2][1]} | {self.board[2][2]}")
 
+    def get(self):
+        return 
 if __name__ == "__main__":
     b = Board()
     b.out()
--- TicTacToe/game.py
+++ TicTacToe/game.py
@@ -11,5 +11,23 @@ class Game:
         row = input(f"Player {pname}, please enter the row of your move (1, 2, 3)(0 to escape): ")
         if row == 0:
             return
-        if col not in (1, 2, 3) or row not in (1, 2, 3):
+        if col not in ('1', '2', '3') or row not in ('1', '2', '3'):
             print("\033[31mPlease enter a valid space.\033[0m")
+            return
+        col = int(col)
+        row = int(row)
+        status = self.board.put(col-1, row-1)
+        if status == 2:
+            self.board.out()
+            print(f"Player {pname} wins!")
+            self.finished = True
+        elif status == 1:
+            self.board.out()
+            print("Board full. Game ends in a tie.")
+            self.finished = True
+
+if __name__=="__main__":
+    g = Game()
+    while(not g.finished):
+        g.board.out()
+        g.turn()
