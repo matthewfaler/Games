e38a6c7a0b1b0badfcb2ffa7a4205d14eff4213e
matthewfaler <matthewfaler1@gmail.com>
2025-09-04 22:51:10 -0400

    Added color to error output for invalid user input

--- Connect4/game.py
+++ Connect4/game.py
@@ -10,7 +10,7 @@ class Game:
         colmap = {'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6}
 
         if inp not in colmap:
-            print("Please choose a valid option.")
+            print("\033[31mPlease choose a valid option.\033[0m")
             return
 
         status = self.board.put(colmap[inp])
